7898283c443df1758f29ae5c86ea7b98481028b4
Imran01000 <imshaik01000@gmail.com>
2020-02-26 09:51:56 +0530

    ADDED:expression a*b+c.

--- Sortingarithmetic.sh
+++ Sortingarithmetic.sh
@@ -4,4 +4,6 @@ read -p "Enter the three numbers" a;
 read b;
 read c;
 result=`echo "scale=3; $a+$b*$c" | bc -l`;
-echo "result is $result";
+echo "1.expression a+b*c result is $result";
+result1=`echo "scale=3; $a*$b+$c" | bc -l`;
+echo "2.expression a*b+c result is $result1";
