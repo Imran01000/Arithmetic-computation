f6755720d711ae93d01d67f2eec6fdca2657baab
Imran01000 <imshaik01000@gmail.com>
2020-02-26 10:24:07 +0530

    ADDED:Stored every expression in dictionary.

--- Sortingarithmetic.sh
+++ Sortingarithmetic.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 #Taking three inputs and print it.
+declare -A expression
 read -p "Enter the three numbers" a;
 read b;
 read c;
@@ -12,4 +13,9 @@ result2=`echo "scale=3; $c+$a/$b" | bc -l`;
 echo "3.expression c+a/b result is $result2";
 result3=`echo "scale=3; $a%$b+$c" | bc -l`;
 echo "4.expression a%b+c result is $result3";
-
+expression[1]="$result";
+expression[2]="$result1";
+expression[3]="$result2";
+expression[4]="$result3";
+expression[5]="$result4";
+echo "${expression[@]}"
