05f0cd856bea7f4876c36661f050864a9ed8afe1
Imran01000 <imshaik01000@gmail.com>
2020-02-26 12:27:14 +0530

    ADDED:Values are sorted in descending format.

--- Sortingarithmetic.sh
+++ Sortingarithmetic.sh
@@ -30,3 +30,19 @@ do
 	expressionArray[$itr]="${expression[$itr]}";
 done
 echo "${expressionArray[@]}"
+
+#Sorting the value in descending order.
+for((itr=1;itr<=4;itr++))
+do 
+   for((inr=1;inr<=3;inr++))
+   do
+      if ((`echo "${expressionArray[$inr]} < ${expressionArray[$inr+1]}" | bc -l`))
+      then
+         temp="${expressionArray[$inr]}";
+         expressionArray[$inr]="${expressionArray[$inr+1]}";
+         expressionArray[$inr+1]=$temp;
+      fi
+   done
+done
+echo "${expressionArray[@]}"
+
