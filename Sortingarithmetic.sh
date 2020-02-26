d502ade3795fe900f6599b9bc06edd0ab8088871
Imran01000 <imshaik01000@gmail.com>
2020-02-26 10:05:14 +0530

    ADDED:3.expression c+a/b.

--- Sortingarithmetic.sh
+++ Sortingarithmetic.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -x
 #Taking three inputs and print it.
-read -p "Enter the three numbers" a;
+echo -p "Enter the three numbers" a;
 read b;
 read c;
+echo "$a , $b , $c ";
 result=`echo "scale=3; $a+$b*$c" | bc -l`;
 echo "1.expression a+b*c result is $result";
 result1=`echo "scale=3; $a*$b+$c" | bc -l`;
 echo "2.expression a*b+c result is $result1";
+result2=`echo "scale=3; $c+$a/$b" | bc -l`;
+echo "3.expression c+a/b result is $result2";
